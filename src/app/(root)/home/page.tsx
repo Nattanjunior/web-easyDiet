a43461a654fcff9675dfab7d5c8e7be6d7ad6d00
nattanjunior <fnatanieljunior@gmail.com>
2024-10-06 16:37:53 -0300

    created folders stepOne and createDiet

--- src/app/(root)/home/page.tsx
+++ src/app/(root)/home/page.tsx
@@ -1,6 +1,15 @@
+'use client'
+
 import Image from "next/image";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    router.push('/stepOne')
+  }
+
   return (
     <main className="w-full h-screen flex justify-center ">
       <div className="w-full text-center flex flex-col justify-center items-center">
@@ -13,7 +22,15 @@ export default function Home() {
 
         <p className="w-[25.1rem] font-medium text-[1.8rem] text-white">Sua dieta personalizada com inteligência artificial 🍴 </p>
 
-        <button className="bg-blueButton text-[1.6rem] text-white font-semibold w-[80%] h-[4.4rem] rounded-[0.8rem] mt-[2.6rem] hover:cursor-pointer hover:translate-y-[-1.0rem] transition-all">Gerar Dieta</button>
+
+        <button
+          onClick={handleNavigation}
+          className="bg-blueButton text-[1.6rem] text-white font-semibold w-[80%] h-[4.4rem] rounded-[0.8rem] mt-[2.6rem] hover:cursor-pointer hover:translate-y-[-1.0rem] transition-all">
+          Gerar Dieta
+        </button>
+
+
+
 
       </div>
 
---
+++ src/app/(root)/stepOne/page.tsx
@@ -0,0 +1,92 @@
+'use client'
+
+import { Header } from "@/components/header/page";
+import { z } from 'zod'
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Input } from "@/components/input/page";
+import { useDataStore } from "../../../../store/data";
+import { useRouter } from 'next/navigation';
+
+
+const schema = z.object({
+  name: z.string().min(1, { message: "Nome é obrigatório" }),
+  weight: z.string().min(1, { message: "Peso é obrigatório" }),
+  age: z.string().min(1, { message: "Idade é obrigatória" }),
+  height: z.string().min(1, { message: "Altura é obrigatória" }),
+})
+
+type FormData = z.infer<typeof schema>
+
+export default function StepOne() {
+
+  const { control, handleSubmit, formState: { errors, isValid } } = useForm<FormData>({
+    resolver: zodResolver(schema)
+  });
+
+  const SetPageOne = useDataStore(state => state.setPageOne)
+
+  const router = useRouter();
+
+  const handleNavigation = (data: FormData) => {
+    SetPageOne({
+      name: data.name,
+      age: data.age,
+      height: data.height,
+      weight: data.weight
+    })
+    router.push('/stepOne')
+  }
+
+  return (
+    <div>
+      <Header
+        step="Passo 1"
+        title="Vamos Começar"
+      />
+
+      <div className="p-[1.6rem] flex flex-col">
+        <p className="text-white">Nome:</p>
+        <Input
+          name="name"
+          control={control}
+          placeholder="Digite seu nome..."
+          error={errors.name?.message}
+        />
+
+        <p className="text-white">Seu peso atual:</p>
+        <Input
+          name="weight"
+          control={control}
+          placeholder="Ex: 75"
+          error={errors.weight?.message}
+        />
+
+        <p className="text-white">Sua altura atual:</p>
+        <Input
+          name="height"
+          control={control}
+          placeholder="Ex: 1.80"
+          error={errors.height?.message}
+        />
+
+        <p className="text-white">Sua idade atual:</p>
+        <Input
+          name="age"
+          control={control}
+          placeholder="Ex: 20"
+          error={errors.age?.message}
+        />
+
+        <button
+          onClick={handleSubmit(handleNavigation)}
+          className="bg-blueButton text-[1.6rem] text-white font-semibold w-[80%] h-[4.4rem] rounded-[0.8rem] mt-[2.6rem] hover:cursor-pointer hover:translate-y-[-1.0rem] transition-all ">
+          Avançar
+        </button>
+
+      </div>
+
+
+    </div>
+  )
+}
