6ea2343b82dacfe404c2567f28fc71d9ce8d606a
nattanjunior <fnatanieljunior@gmail.com>
2024-10-06 16:35:48 -0300

    created input

---
+++ src/components/header/page.tsx
@@ -0,0 +1,29 @@
+import { ArrowLeft } from 'lucide-react';
+import { LoaderCircle } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
+interface HeaderProps {
+  step: string;
+  title: string;
+}
+
+export function Header({ step, title }: HeaderProps) {
+  const router = useRouter();
+
+  const handleNavigationBack = () => {
+    router.push('/home')
+  }
+
+  return (
+    <div className='bg-white p-4 pb-9 rounded-bl-2xl rounded-br-2xl '>
+      <div className='flex items-center gap-[.6rem] mb-[.8rem]'>
+        <ArrowLeft size={30} onClick={handleNavigationBack} />
+        <p>{step}</p>
+        <LoaderCircle />
+      </div>
+
+      <h1 className='text-[2.78rem] font-semibold'>{title}</h1>
+    </div>
+  )
+
+}
---
+++ src/components/input/page.tsx
@@ -0,0 +1,37 @@
+import { Controller } from 'react-hook-form';
+
+
+interface InputProps {
+  name: string;
+  control: any;
+  placeholder?: string;
+  rules?: object;
+  error?: string;
+}
+
+
+export function Input({ name, control, placeholder, rules, error }: InputProps) {
+  return (
+    <div className='mb-[1.6rem]'>
+      <Controller
+        name={name}
+        control={control}
+        rules={rules}
+
+        render={({ field: { onChange, onBlur, value } }) => (
+          <input type='text'
+            className='h-[4.4rem] w-[100%] bg-white p-3 rounded-md outline-none'
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            onBlur={onBlur}
+          />
+        )}
+      />
+      {error && (
+        <p className='text-red-600 mt-1'>{error}</p>
+      )}
+
+    </div>
+  )
+}
