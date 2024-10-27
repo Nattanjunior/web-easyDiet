07c1b8447ec95ebbf956e913a0658a4cfb9dd466
nattanjunior <fnatanieljunior@gmail.com>
2024-10-26 21:34:05 -0300

    icone removido e cor de fundo muda conforme responsividade muda

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -17,11 +17,11 @@ export function Header({ step, title }: HeaderProps) {
   }
 
   return (
-    <header className='bg-white p-4 pb-9 rounded-bl-2xl rounded-br-2xl '>
+    <header className='bg-white p-4 pb-9 rounded-bl-2xl rounded-br-2xl md:bg-black md:text-white'>
       <div className='flex items-center gap-[.6rem] mb-[.8rem]'>
         <ArrowLeft size={30} onClick={handleNavigationBack} />
         <p>{step}</p>
-        <LoaderCircle />
+        {/* <LoaderCircle /> */}
       </div>
 
       <h1 className='text-[2.78rem] font-semibold'>{title}</h1>
