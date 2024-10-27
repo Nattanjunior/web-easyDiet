80daf52f868897fb40379159431478b8736983bd
nattanjunior <fnatanieljunior@gmail.com>
2024-10-27 17:52:41 -0300

    modificacoes na seta de voltar, adicionado o texto 'voltar' e modificacoes na responsividade

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,15 +1,13 @@
 'use client'
 
 import { ArrowLeft } from 'lucide-react';
-import { LoaderCircle } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
 interface HeaderProps {
-  step?: string;
   title: string;
 }
 
-export function Header({ step, title }: HeaderProps) {
+export function Header({ title }: HeaderProps) {
   const router = useRouter();
 
   const handleNavigationBack = () => {
@@ -17,11 +15,11 @@ export function Header({ step, title }: HeaderProps) {
   }
 
   return (
-    <header className='bg-white p-4 pb-9 rounded-bl-2xl rounded-br-2xl md:bg-black md:text-white'>
+    <header className='bg-white p-4 pb-9 rounded-bl-2xl rounded-br-2xl sm:bg-black sm:text-white md:bg-black md:text-white'>
       <div className='flex items-center gap-[.6rem] mb-[.8rem]'>
-        <ArrowLeft size={30} onClick={handleNavigationBack} />
-        <p>{step}</p>
-        {/* <LoaderCircle /> */}
+        <ArrowLeft size={30} onClick={handleNavigationBack} className='md:w-[4rem] md:h-[4rem]'/>
+        <p className='md:text-[2.5rem] text-[2rem]' onClick={handleNavigationBack} >Voltar</p>
+        
       </div>
 
       <h1 className='text-[2.78rem] font-semibold'>{title}</h1>
