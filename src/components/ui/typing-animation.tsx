631962725963ac2271d316cb06e1baa5a9399134
nattanjunior <fnatanieljunior@gmail.com>
2024-10-27 17:50:48 -0300

    componente de animacao de texto instalado

---
+++ src/components/ui/typing-animation.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { cn } from "@/lib/utils";
+
+interface TypingAnimationProps {
+  text: string;
+  duration?: number;
+  className?: string;
+}
+
+export default function TypingAnimation({
+  text,
+  duration = 60,
+  className,
+}: TypingAnimationProps) {
+  const [displayedText, setDisplayedText] = useState<string>("");
+  const [i, setI] = useState<number>(0);
+
+  useEffect(() => {
+    const typingEffect = setInterval(() => {
+      if (i < text.length) {
+        setDisplayedText(text.substring(0, i + 1));
+        setI(i + 1);
+      } else {
+        clearInterval(typingEffect);
+      }
+    }, duration);
+
+    return () => {
+      clearInterval(typingEffect);
+    };
+  }, [duration, i]);
+
+  return (
+    <h1
+      className={cn(
+        "text-white text-center w-[25.1rem] text-[1.8rem] font-bold lg:w-[35rem] lg:text-[2.5rem] mt-[-1rem]",
+        className,
+      )}
+    >
+      {displayedText ? displayedText : text}
+    </h1>
+  );
+}
